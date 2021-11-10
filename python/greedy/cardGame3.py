3383d99196b617c683eaea9b6c846106f7a46ebe
김지웅 <godjw96@gmail.com>
2021-11-11 03:55:42 +0900

    Create cardGame3.py

---
+++ python/greedy/cardGame3.py
@@ -0,0 +1,13 @@
+# 2중 반복문 구조를 이용하는 답안 예시
+n, m = map(int, input().split())
+
+result = 0
+# 한 줄 씩 입력받아 확인
+for i in range(n):
+    data = list(map(int, input().split()))
+    # 현재 줄에서 가장 작은 수 찾기
+    min_value = 10001
+    for a in data:
+        min_value = min(min_value, a)
+    # 가장 작은 수 들 중에서 가장 큰 수 찾기
+    result = max(result, min_value)
