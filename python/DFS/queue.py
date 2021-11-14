2a208a35e70de06f6e0f35dfc32bd8a18458b162
김지웅 <godjw96@gmail.com>
2021-11-14 10:14:40 +0900

    Create queue.py

---
+++ python/DFS/queue.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+# 큐 구현을 위해 deque 라이브러리 사용
+
+queue = deque()
+
+queue.append(5)
+queue.append(2)
+queue.append(3)
+queue.append(7)
+queue.popleft()
+queue.append(1)
+queue.append(4)
+queue.popleft()
+
+print(queue) # 먼저 들어온 순서대로 출력
+queue.reverse()
+print(queue)  # 나중에 들어온 순서대로 출력
+
+
