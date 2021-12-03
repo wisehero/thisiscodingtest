f552404c12ab05777340b92537fe891c0edcd52d
김지웅 <godjw96@gmail.com>
2021-12-03 18:01:13 +0900

    Create three_sum.py

---
+++ python_coiding_test/linear_data_structure/three_sum.py
@@ -0,0 +1,56 @@
+# 배열을 입력받아 합으로 0을 만들 수 있는 3개의 엘리먼트를 출력하라.
+# nums = [-1, 0, 1, 2, -1, 4]
+# 출력
+# [
+#   [-1, 0, 1]
+#   [-1, -1, 2]
+# ]
+
+def three_sum_brute_force(nums):
+    results = []
+    nums.sort()  # 먼저 정렬을 해준다.
+
+    for i in range(len(nums) - 2):
+        # 중복된 값 건너 뛰기
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        for j in range(i + 1, len(nums) - 1):
+            if j > i + 1 and nums[j] == nums[j - 1]:
+                continue
+            for k in range(j + 1, len(nums)):
+                if k > j + 1 and nums[k] == nums[k - 1]:
+                    continue
+                if nums[i] + nums[j] + nums[k] == 0:
+                    results.append([nums[i], nums[j], nums[k]])
+
+    return results
+
+
+def three_sum_two_pointer(nums):
+    results = []
+    nums.sort()
+
+    for i in range(len(nums) - 2):
+        # 중복된 값 건너 뛰기
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+
+        # 간격을 좁혀가며 합 계산
+        left, right = i + 1, len(nums) - 1
+        while left < right:
+            sum = nums[i] + nums[left] + nums[right]
+            if sum < 0:
+                left += 1
+            elif sum > 0:
+                right -= 1
+            else:
+                # sum = 0인 경우이므로 정답 및 스킵 처리
+                results.append([nums[i], nums[left], nums[right]])
+
+                while left < right and nums[left] == nums[left + 1]:
+                    left += 1
+                while left < right and nums[right] == nums[right + 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+    return results
