583f83d26b1769191d5d6cab275b43880541ccda
김지웅 <godjw96@gmail.com>
2021-12-03 18:01:21 +0900

    Create most_common_word.py

---
+++ python_coiding_test/strings/most_common_word.py
@@ -0,0 +1,11 @@
+import collections
+import re
+
+
+def most_common_word(paragraph, banned):
+    words = [word for word in re.sub(r'[^\w]', ' ', paragraph)
+        .lower().split() if word not in banned]
+
+    counts = collections.Counter(words)
+    # 가장 흔하게 등장하는 단어의 첫 번째 인덱스 리턴
+    return counts.most_common(1)[0][0]
