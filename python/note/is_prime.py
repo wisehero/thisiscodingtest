ed4cf58fd3436a06824ec7d4bd75fa6bab24b260
김지웅 <godjw96@gmail.com>
2021-12-14 21:29:34 +0900

    Update is_prime.py

--- python/note/is_prime.py
+++ python/note/is_prime.py
@@ -3,10 +3,12 @@ import math
 
 
 def is_prime_number(x):
+    if x == 3:
+        return True
     for i in range(2, int(math.sqrt(x)) + 1):
         if x % i == 0:
             return False
-        return True
+    return True
 
 
 def is_prime_numberV2(x):
@@ -14,3 +16,6 @@ def is_prime_numberV2(x):
         if x % i == 0:
             return False
     return True
+
+
+
