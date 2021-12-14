988501c27cac63c67420dd1e0d0c56737618de3f
김지웅 <godjw96@gmail.com>
2021-12-14 17:56:37 +0900

    Create 2839.py

---
+++ python/backjoon/class2/2839.py
@@ -0,0 +1,13 @@
+n = int(input())  # 설탕
+
+result = 0  # 봉지 수
+
+while n >= 0:
+    if n % 5 == 0:  # 5로 나눈 나머지가 0인 경우
+        result += n // 5  # 5로 나눈 몫 추가
+        print(result)
+        break
+    n -= 3  # 설탕이 5의 배수가 될때까지 반복
+    result += 1  # 봉지 추가
+else:
+    print(-1)  # while문이 거짓이 되면 -1 출력
