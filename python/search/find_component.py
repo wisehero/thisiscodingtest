6fa0121e5baf85e5f8b1a15047bbb1fce5e55f86
김지웅 <godjw96@gmail.com>
2022-03-12 15:35:51 +0900

    Update find_component.py

--- python/search/find_component.py
+++ python/search/find_component.py
@@ -15,7 +15,7 @@ def binary_search(array, target, start, end):
         # 중간 점의 값보다 찾고자 하는 값이 큰 경우 오른쪽 확인
         else:
             start = mid + 1
-        return None
+    return None
 
 
 n = int(sys.stdin.readline().rstrip())
