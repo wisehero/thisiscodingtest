365533f38b09c1a6d28c649061caf5062a6ef90d
wisehero10 <godjw96@gmail.com>
2022-07-07 17:25:39 +0900

    # 로그 파일 재정렬

---
+++ python_coiding_test/1.StringOperation/3_reorder_log_files/reorder_log_files.py
@@ -0,0 +1,21 @@
+"""
+로그를 재정렬 하라. 기준은 다음과 같다.
+
+1. 로그의 가장 앞 부분은 식별자다.
+2. 문자로 구성된 로그가 숫자 로그보다 앞에 온다.
+3. 식별자는 순서에 영향을 끼치지 않지만, 문자가 동일할 경우 식별자 순으로 한다.
+4. 숫자 로그는 입력 순서대로 한다.
+"""
+
+
+def reorder_log_files(self, logs):
+    letters, digits = [], []
+    for log in logs:
+        if log.split()[1].isdigit():
+            digits.append(log)
+        else:
+            letters.append(log)
+
+    # 2개의 키를 람다 표현식으로 정렬
+    letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+    return letters + digits
