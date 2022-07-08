ca912c13c3cf3d45f41f03afacd71c903eb4f47e
wisehero10 <godjw96@gmail.com>
2022-07-08 18:54:35 +0900

    # 보석과 돌

--- python_coiding_test/5_Hash_Table/1_Jewels_And_Stones/JewelsAndStonesV3.py
+++ python_coiding_test/5_Hash_Table/1_Jewels_And_Stones/JewelsAndStonesV3.py
@@ -1,6 +1,9 @@
 """
 J는 보석이며, S는 갖고 있는 돌이다. S에는 보석이 몇개나 있을까?
 대소문자는 구분한다.
+
+입력 : J = "aA", S = "aAAbbbb"
+출력 : 3
 """
 import collections
 
@@ -13,3 +16,4 @@ def numJewelsInStones(J, S):
         count += freqs[char]
 
     return count
+
