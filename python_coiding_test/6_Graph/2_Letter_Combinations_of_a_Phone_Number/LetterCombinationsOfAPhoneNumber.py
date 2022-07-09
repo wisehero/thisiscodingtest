e6295cafbce0babef730ef6e4f785e3f343f37f1
wisehero10 <godjw96@gmail.com>
2022-07-09 22:44:25 +0900

    # 전화 번호 문자 조합

---
+++ python_coiding_test/6_Graph/2_Letter_Combinations_of_a_Phone_Number/LetterCombinationsOfAPhoneNumber.py
@@ -0,0 +1,34 @@
+"""
+2에서 9까지의 숫자가 주어졌을 때 전화 번호로 조합 가능한 모든 문자를 출력하라.
+
+Input: digits = "23"
+Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
+Input: digits = "2"
+Output: ["a","b","c"]
+"""
+
+
+def letterCombinations(digits):
+    def dfs(index, path):
+        # 끝까지 탐색하면 백트래킹
+        if len(path) == len(digits):
+            result.append(path)
+            return
+
+        # 입력값 자릿수 단위 반복
+        for i in range(index, len(digits)):
+            # 숫자에 해당하는 모든 문자열 반복
+            for j in dic[digits[i]]:
+                dfs(i + 1, path + j)
+
+    # 예외 처리
+    if not digits:
+        return []
+
+    dic = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+           "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+    result = []
+    dfs(0, "")
+
+    return result
