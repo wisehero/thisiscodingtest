5bd78d370612af98171914c9873141bd55cdf102
wisehero10 <godjw96@gmail.com>
2022-08-29 23:37:48 +0900

    Create 소트인사이드.py

---
+++ doit/_04_sort/소트인사이드.py
@@ -0,0 +1,18 @@
+import sys
+
+print = sys.stdout.write
+A = list(input())
+
+for i in range(len(A)):
+    Max = i
+    for j in range(i + 1, len(A)):
+        # 최댓값 찾기
+        if A[j] > A[Max]:
+            Max = j
+    if A[i] < A[Max]:
+        temp = A[i]
+        A[i] = A[Max]
+        A[Max] = temp
+
+for i in range(len(A)):
+    print(A[i])
